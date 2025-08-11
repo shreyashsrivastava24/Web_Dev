292e088b8a53e49bbaa9141b06d6546317292045
shreyashsrivastava24 <shreyashsrivastava010@gmail.com>
2025-08-12 00:12:27 +0530

    updated

---
+++ J1_Intro/new.js
@@ -0,0 +1,8 @@
+// ctrl l se terminal screen clear
+// ctrl ` se terminal open/close
+// node js backend me javascript ko use krti and script.js se index.html me use  krte wo frontend k liye
+
+console.log("Hello World")
+console.log("Code is running...")
+console.log("Code is also running...")
+console.log("Code is running like a wow...")
---
+++ J1_Intro/script.js
@@ -0,0 +1,32 @@
+// vs code k terminal me iss code ko run krne pe error aayega kyuki kuch cheeze sirf browser me available h
+alert("Hello World");
+
+// inspect krke console me dekhenge to dikhega ye,iss se aap dekh skte ki script me execution kha tk pahucha, makes debugging easy
+console.log("Code is running...")
+console.log("Code is also running...")
+console.log("Code is running like a wow...")
+
+// iss se alert msg k bad number puchega agr mai jo no dal du wo a me aa jayega
+var a = prompt("Enter ur number")
+
+// simply as if else
+var isTrue = confirm("Are u sure want to leave this page and blast ur computer?")
+
+if(isTrue) {
+    console.log("Computer is blasting")
+}
+
+else {
+    console.log("Computer is not blasting!")
+}
+
+// ab mai koi no dalu to wo console me aa jayega
+console.log("Your number is " + a)
+
+// styling v change kr skte
+
+// iss se title change ho jayega
+document.title = "Hey I am Good"
+
+// bg color red kr dega...ye inline css add krta h
+document.body.style.backgroundColor = "red"
